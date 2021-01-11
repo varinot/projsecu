c41c41aefb918ed426cfa1ad042e19bd1a17d698
Philippe <varinotformation88@gmail.com>
2021-01-11 14:59:59 +0100

    commit initial master

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+   
+        
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+   }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $userAdmin = new User();
+        $userAdmin->setEmail('bobo@orange.fr');
+        $userAdmin->setRoles(["ROLE_USER","ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->passwordEncoder->encodePassword($userAdmin,'passadmin'
+         ));
+
+        $user = new User();
+       $user->setEmail('baba@orange.fr');
+       $user->setRoles(["ROLE_USER"]);
+       $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,'password'
+        ));
+                 $manager->persist($user);
+
+        $manager->flush();
+    }
+}
