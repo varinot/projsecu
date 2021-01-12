96da14447d7c071f0f6f6caea284964eecb7692a
Philippe <varinotformation88@gmail.com>
2021-01-12 17:04:56 +0100

    twig.yaml et MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/main/{id}", name="main")
      */
-    public function index(): Response
+    public function index($id): Response
     {
+        $userRepository = $this->getdoctrine()->getRepository(User::class);
+        $users = $userRepository->find($id);
+       // return $this->$array[];
+        //if(user.roles === ["ROLE_USER","ROLE_ADMIN"])
+
     return $this->render('main/index.html.twig', [
         'controller_name' => 'MainController',]);
     }  
