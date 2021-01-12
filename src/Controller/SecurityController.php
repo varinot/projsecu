ccb5bd604bff72c3f8d97d6cde33f7630d7de2d4
Philippe <varinotformation88@gmail.com>
2021-01-12 10:54:15 +0100

    creation aiguillage main pour consultation produits

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,10 +14,7 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-            return $this->redirectToRoute('profil');
-         }
-
+       
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
@@ -32,5 +29,6 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    } 
+
 }
