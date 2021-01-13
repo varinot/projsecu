cb5f3fbbd62d5fe0cd06a1b7aa6eccd6596323bc
Philippe <varinotformation88@gmail.com>
2021-01-13 12:04:16 +0100

    ajout des fixtures comments

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -21,20 +21,20 @@ class UserFixtures extends Fixture
     {
 
         $userAdmin = new User();
-        $userAdmin->setEmail('bobo@orange.fr');
+        $userAdmin->setEmail('adbobo@example.com');
         $userAdmin->setRoles(["ROLE_USER","ROLE_ADMIN"]);
-        $userAdmin->setPassword($this->passwordEncoder->encodePassword($userAdmin,'passadmin'
+        $userAdmin->setPassword($this->passwordEncoder->encodePassword($userAdmin,'adminutil'
          ));
          $manager->persist($userAdmin);
          $manager->flush();
 
-       $user = new User();
-       $user->setEmail('baba@orange.fr');
-       $user->setRoles(["ROLE_USER"]);
-       $user->setPassword($this->passwordEncoder->encodePassword(
-            $user,'password'
+       $user1 = new User();
+       $user1->setEmail('baba@example.fr');
+       $user1->setRoles(["ROLE_USER"]);
+       $user1->setPassword($this->passwordEncoder->encodePassword(
+            $user1,'passutil'
         ));
-        $manager->persist($user);
+        $manager->persist($user1);
         $manager->flush();
     }
 }
--- src/Entity/Comments.php
+++ src/Entity/Comments.php
@@ -37,6 +37,12 @@ class Comments
      */
     private $updateAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +92,18 @@ class Comments
     public function setUpdateAt(\DateTimeInterface $updateAt): self
     {
         $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
